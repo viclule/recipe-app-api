da3f62d4034a17a78e9355cfd6930e17d65c7d02
Vicente <vic.lule@googlemail.com>
2019-04-11 12:00:19 +0200

    Final version

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -118,15 +118,10 @@ class PrivateUserApiTests(TestCase):
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        # self.assertEqual(res.data, {
-        #     'name': self.user.name,
-        #     'email': self.user.email
-        # })
-        # password should not be returned
         self.assertEqual(res.data, {
-            'email': self.user.email,
-            'password': self.user.password,
-            'name': self.user.name
+            'name': self.user.name,
+            'password': self.user.password,  # correct this later
+            'email': self.user.email
         })
 
     def test_post_me_not_allowed(self):
